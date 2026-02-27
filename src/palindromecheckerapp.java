495325644dead3b07579aa266bd2c97760f683e5
AG-0015 <agajalakshmi07@gmail.com>
2026-02-27 16:02:16 +0530

    UC4

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -2,17 +2,18 @@ public class palindromecheckerapp {
 
     public static void main(String[] args) {
 
-        String original = "radar";
+        String original = "Madam";
+        String lower = original.toLowerCase();
         String reversed = "";
 
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed += original.charAt(i);
+        for (int i = lower.length() - 1; i >= 0; i--) {
+            reversed += lower.charAt(i);
         }
 
-        if (original.equals(reversed)) {
-            System.out.println("Palindrome");
+        if (lower.equals(reversed)) {
+            System.out.println(original + " is Palindrome (Case-Insensitive)");
         } else {
-            System.out.println("Not Palindrome");
+            System.out.println(original + " is NOT Palindrome");
         }
     }
 }
