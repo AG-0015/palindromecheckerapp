dad04fdc2dfe347d327c9f4b685b3aeb0016afad
AG-0015 <agajalakshmi07@gmail.com>
2026-02-27 15:57:37 +0530

    UC3

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -1,39 +1,18 @@
-import java.util.Deque;
-import java.util.ArrayDeque;
 public class palindromecheckerapp {
 
     public static void main(String[] args) {
 
-        // Original String
-        String original = "racecar";
+        String original = "radar";
+        String reversed = "";
 
-        // Create Deque
-        Deque<Character> deque = new ArrayDeque<>();
-
-        // Insert characters into deque
-        for (int i = 0; i < original.length(); i++) {
-            deque.addLast(original.charAt(i));   // Insert at rear
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed += original.charAt(i);
         }
 
-        boolean isPalindrome = true;
-
-        // Compare front and rear until deque size <= 1
-        while (deque.size() > 1) {
-
-            char front = deque.removeFirst();  // Remove from front
-            char rear  = deque.removeLast();   // Remove from rear
-
-            if (front != rear) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        // Display result
-        if (isPalindrome) {
-            System.out.println("The string \"" + original + "\" is a Palindrome (Using Deque).");
+        if (original.equals(reversed)) {
+            System.out.println("Palindrome");
         } else {
-            System.out.println("The string \"" + original + "\" is NOT a Palindrome.");
+            System.out.println("Not Palindrome");
         }
     }
 }
